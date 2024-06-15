f6d09655668fee125de3f8dedd74aadfc10fb337
princesslunaequestrian <backgroundpony@outlook.com>
2024-06-15 07:28:55 +0300

    rotation testing

--- converter.go
+++ converter.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/fogleman/gg"
+	"github.com/nfnt/resize"
+)
+
+// Gets the image by the `filePath`, rotates it by `d` degrees until a complete loop and returns the directory where the rotated images are stored.
+func RotateImage(filePath string, d int) (string, error) {
+
+	img, err := gg.LoadImage("./t.png")
+	if err != nil {
+		return "", err
+	}
+
+	newWidth := 500
+	newHeight := 500
+
+	res_img := resize.Resize(uint(newWidth), uint(newHeight), img, resize.Lanczos2)
+
+	// dc.RotateAbout(1, 1000/2, 1000/2)
+	for i := range 45 {
+		dc := gg.NewContext(1000, 1000)
+		dc.RotateAbout(gg.Radians(float64(d*(i+1))), 500, 500)
+		dc.DrawImage(res_img, 250, 250)
+		dc.SavePNG(fmt.Sprintf("./tests/test_%d.png", i))
+	}
+
+	return "./tests/", nil
+}
+
+func StackImage() (string, error) {
+
+	return "", nil
+}
--- converter_test.go
+++ converter_test.go
@@ -1,42 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/fogleman/gg"
 	"github.com/nfnt/resize"
 )
 
-func TestLoadImage(t *testing.T) {
+func TestRotateImage(t *testing.T) {
 	img, err := gg.LoadImage("./t.png")
 	if err != nil {
 		t.Error(err)
 	}
-
-	width := img.Bounds().Dx()
-	height := img.Bounds().Dy()
 
 	newWidth := 500
 	newHeight := 500
 
 	res_img := resize.Resize(uint(newWidth), uint(newHeight), img, resize.Lanczos2)
 
-	width = res_img.Bounds().Dx()
-	height = res_img.Bounds().Dy()
-
-	t.Log(width, height)
-
-	dc := gg.NewContext(1000, 1000)
 	// dc.RotateAbout(1, 1000/2, 1000/2)
-	dc.RotateAbout(1, 500, 500)
-	dc.DrawImage(res_img, 250, 250)
-	dc.SavePNG("./test.png")
+	for i := range 45 {
+		dc := gg.NewContext(1000, 1000)
+		dc.RotateAbout(gg.Radians(float64(8*(i+1))), 500, 500)
+		dc.DrawImage(res_img, 250, 250)
+		dc.SavePNG(fmt.Sprintf("./tests/test_%d.png", i))
+	}
 }
 
-// func TestRotateImage(t *testing.T) {
-// 	img, err := gg.LoadImage("./Assets/Images/Disk.png")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	img.Rotate
-// }
+func TestStackImage(t *testing.T) {
+	img1, err := gg.LoadImage("./t.png")
+	if err != nil {
+		t.Error(err)
+	}
+
+	back1, err := gg.LoadImage("./Assets/Images/Disk.png")
+
+	b1s := resize.Resize(1000, 1000, back1, resize.Lanczos3)
+	i1s := resize.Resize(500, 500, img1, resize.Lanczos3)
+
+	dc := gg.NewContext(1000, 1000)
+	dc.DrawImage(i1s, 250, 250)
+	dc.DrawImage(b1s, 0, 0)
+	dc.SavePNG("./test.png")
+}
